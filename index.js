bbb67f998bcc08eaa929b0d9ee1d0eb5c3e68f86
Gamel Ayodele <ayodelegamel@gmail.com>
2023-09-08 22:33:07 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/api/", (req, res) => {
+  const { slack_name, track } = req.query;
+
+  if (!slack_name || !track) {
+    return res
+      .status(400)
+      .json({ error: "slack_name and track are required parameters" });
+  }
+  //   Current UTC Time
+  const utc_time = new Date();
+  //   Two minute ahead
+  const twoMinutesAhead = new Date(utc_time);
+  twoMinutesAhead.setUTCMinutes(utc_time.getUTCMinutes() + 2);
+  //   Two minute behind
+  const twoMinutesBehind = new Date(utc_time);
+  twoMinutesBehind.setUTCMinutes(utc_time.getUTCMinutes() - 2);
+
+  if (utc_time >= twoMinutesBehind && utc_time <= twoMinutesAhead) {
+    //   CurrentDay
+    const today = new Date();
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const current_day = daysOfWeek[today.getDay()];
+
+    // GitHub URLs
+    const github_file_url = "https://github.com/gamelayo/endpoint/index.js";
+    const github_repo_url = "https://github.com/gamelayo/endpoint";
+
+    // Response JSON
+    const response = {
+      slack_name,
+      current_day,
+      utc_time,
+      track,
+      github_file_url,
+      github_repo_url,
+      status_code: 200,
+    };
+
+    res.status(200).json(response);
+  } else {
+    return res.status(400).json({ error: "utc_time is incorrect" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
